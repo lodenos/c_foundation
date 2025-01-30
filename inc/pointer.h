0bf1102b94e1c0a616f015948279599b8ecf45ad
lodenos <nikolaiilodenos@gmail.com>
2025-01-30 05:58:48 +0000

    add a malloc wrapper for malloc_ptr

--- inc/pointer.h
+++ inc/pointer.h
@@ -1,11 +1,20 @@
 #ifndef POINTER_H
 #define POINTER_H
 
+#include <stdlib.h>
+#include "type.h"
+
 typedef struct ptr_s ptr_t;
 
 struct ptr_s {
   void* start;
   void* stop;
 };
+
+static __inline__ void* malloc_ptr(ptr_t* ptr, u64_t size) {
+  ptr->start = malloc(size);
+  ptr->stop = (u8_t*)ptr->start + size;
+  return ptr->start;
+}
 
 #endif
