77a251a74d8c3eff0675ee965077f3bbea80841b
lodenos <nikolaiilodenos@gmail.com>
2026-03-22 15:18:08 +0000

    add the 2D and 3D polar definition

---
+++ inc/polar.h
@@ -0,0 +1,62 @@
+#ifndef POLAR_H
+#define POLAR_H
+
+#include "type.h"
+
+struct p32_s {
+  union {
+    struct {
+      f32_t r;
+      f32_t p;
+    };
+    struct {
+      f32_t rho;
+      f32_t phi;
+    };
+  };
+};
+
+struct p64_s {
+  union {
+    struct {
+      f64_t r;
+      f64_t p;
+    };
+    struct {
+      f64_t rho;
+      f64_t phi;
+    };
+  };
+};
+
+struct s32_s {
+  union {
+    struct {
+      f32_t r;
+      f32_t p;
+      f32_t t;
+    };
+    struct {
+      f32_t rho;
+      f32_t phi;
+      f32_t theta;
+    };
+  };
+};
+
+struct s64_s {
+  union {
+    struct {
+      f64_t r;
+      f64_t p;
+      f64_t t;
+    };
+    struct {
+      f64_t rho;
+      f64_t phi;
+      f64_t theta;
+    };
+  };
+};
+
+#endif
